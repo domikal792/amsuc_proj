77f83aec5785c8e6a3fa9e4d92007853d76f43bc
Dominik Kala <dominikkala@gmail.com>
2021-12-27 10:04:50 +0100

    feat: dec_to_7seg formatting

--- src/dec_to_7seg.v
+++ src/dec_to_7seg.v
@@ -4,24 +4,23 @@
 /// @note Copyright (c) 2021 AMSUC - Countdown Timer - Kala, Jaraczewski
 
 module DEC_TO_7SEG(
-    input [3:0] IN, 
-    input DP, 
-    output reg [7:0] OUT
+  input [3:0] IN, 
+  input DP, 
+  output reg [7:0] OUT
 );
-always @(IN) begin
+  always @(IN) begin
     case(IN)
-        4'h0: OUT = {DP, 7'b1000000};
-        4'h1: OUT = {DP, 7'b1111001};
-        4'h2: OUT = {DP, 7'b0100100};
-        4'h3: OUT = {DP, 7'b0110000};
-        4'h4: OUT = {DP, 7'b0011001};
-        4'h5: OUT = {DP, 7'b0010010};
-        4'h6: OUT = {DP, 7'b0000010};
-        4'h7: OUT = {DP, 7'b1111000};
-        4'h8: OUT = {DP, 7'b0000000};
-        4'h9: OUT = {DP, 7'b0010000};
-        default: OUT = 8'b11000000;
+      4'h0: OUT = {DP, 7'b1000000};
+      4'h1: OUT = {DP, 7'b1111001};
+      4'h2: OUT = {DP, 7'b0100100};
+      4'h3: OUT = {DP, 7'b0110000};
+      4'h4: OUT = {DP, 7'b0011001};
+      4'h5: OUT = {DP, 7'b0010010};
+      4'h6: OUT = {DP, 7'b0000010};
+      4'h7: OUT = {DP, 7'b1111000};
+      4'h8: OUT = {DP, 7'b0000000};
+      4'h9: OUT = {DP, 7'b0010000};
+      default: OUT = 8'b11000000;
     endcase 
-end
-
+  end
 endmodule
