0edbef5221301fbd41ef8efd58fa6c9576754f56
Dominik Kala <dominikkala@gmail.com>
2021-12-27 09:29:22 +0100

    feat: Up counter

--- src/up_counter.v
+++ src/up_counter.v
@@ -1,38 +1,31 @@
-module CNT(CLK,CLR,CE,Q,CEO);
-parameter BITS_NUM = 8;
-parameter MOD = 10; 
-input CLK, CLR, CE;
-output reg [BITS_NUM-1:0] Q;
-output CEO;
+`timescale 1ns / 1ps
+/// @file up_counter.v
+///
+/// @note Copyright (c) 2021 ArmCpp - Kala, Jaraczewski
 
-always @(posedge CLK or posedge CLR)
-  if(CLR)
-    Q <= {BITS_NUM{1'b0}};
-  else begin
-    if(CE) begin
-      if(Q != MOD-1)
-        Q <= Q + 1;
-      else
-        Q <= {BITS_NUM{1'b0}};
+module UP_CNT #(
+  parameter BITS_NUM = 8,
+  parameter MODULO = 10
+) (
+  input CLK,
+  input CLR,
+  input CE,
+  output reg [BITS_NUM-1:0] Q,
+  output CEO
+);
+  always @(posedge CLK or posedge CLR) begin
+    if (CLR)
+      Q <= {BITS_NUM{1'b0}};
+    else begin
+      if (CE) begin
+        if (Q != MODULO-1)
+          Q <= Q + 1;
+        else
+          Q <= {BITS_NUM{1'b0}};
+      end
     end
   end
 
-assign CEO = CE & (Q == MOD-1);
+  assign CEO = CE & (Q == MODULO-1);
 
 endmodule
-
-//zadania:
-//1 napisac tb + symulacja
-
-//!!! zbadac wplyw CE; jak dziala CEO?
-
-//2 zrealizowac CEO synchronicznie - przy generowaniu stanu Q
-//3 dodac opoznienia do przypisania 
-
-//!!! jaka jest roznica pomiedzy realizacja 1 i 2 (ew. 3)
-
-//4 zrealizowac polaczenie kaskadowe dwoch sekcji licznika bcd; przeniesienie ma byc zegarem nastepnej sekcji
-
-//!!! wnioski
-
-//5 realizacja synchroniczna --- specyfika ukladow FPGA; SKEW
