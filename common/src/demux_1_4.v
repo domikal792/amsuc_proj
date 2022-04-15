3ed1ed8eb24e48b08db3a698aa5d5818b4d73afb
Dawid <jaraczewskidawid@gmail.com>
2022-04-15 23:55:47 +0200

    feat: add comment to common/src/demux_1_4.v test

--- common/src/demux_1_4.v
+++ common/src/demux_1_4.v
@@ -3,6 +3,17 @@
 ///
 /// @note Copyright (c) 2022 AMSUC - Dominik Kala i Dawid Jaraczewski
 
+/// Demultiplekser 1 na 4
+///
+/// @tparam BITS_NUM - Liczba bitow danych wejsciowych.
+///
+/// @param [IN] X - Dane wejściowe.
+/// @param [IN] SEL - Wybór wyjścia.
+/// @param [OUT] Y0 - Wyjście 0
+/// @param [OUT] Y1 - Wyjście 1
+/// @param [OUT] Y2 - Wyjście 2
+/// @param [OUT] Y3 - Wyjście 3
+
 module DEMUX_1_4 #(
   parameter BITS_NUM = 1
 ) (
